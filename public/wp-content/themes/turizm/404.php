5060c2a7240d1e8001446ab7ab13899b3d72a61e
Yuliya Volkova <yuliyavolkova@gmail.com>
2018-07-30 23:07:58 +0300

    'reload'

--- public/wp-content/themes/turizm/404.php
+++ public/wp-content/themes/turizm/404.php
@@ -7,18 +7,16 @@ Template Name: Home Page
 <!DOCTYPE html>
 <html lang="ru">
 <head>
-    <title>Страница поста</title>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
-    <link rel="stylesheet" href="/css/libs.min.css">
-    <link rel="stylesheet" href="/css/main.css">
-    <link rel="stylesheet" href="/css/media.css">
+	<title>Страница поста</title>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+	<?php wp_head(); ?>
 </head>
 <body>
 <div class="wrapper">
-    <h3 class="title-page">Запрашиваемая вами страница не найдена</h3>
-    <a href="/" class="btn-read-post">Перейти на Главную</a>
+	<h3 class="title-page">Запрашиваемая вами страница не найдена</h3>
+	<a href="/" class="btn-read-post">Перейти на Главную</a>
 </div>
 </body>
 </html>
